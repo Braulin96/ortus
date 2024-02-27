c510200ec3c36d10b88923809ebb703b4a40da27
Braulin Pires <braulinpires@hotmail.com>
2024-02-27 09:09:26 +0000

    organised the divs

--- src/components/JourneyStatistics.js
+++ src/components/JourneyStatistics.js
@@ -1,26 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const JourneyStatistics = () => {
   return (
-    <div>
-        <div className="flex">
-            <div>
-                Maps
-
-            </div>
-            <div>
-                <div>
-                    graphic 1
-
-                </div>
-                <div>
-                graphic 2
-                </div>
-
-            </div>
-
+    <div className="w-100" style={{width:"100%"}}>
+      <div className="flex justify-between px-4 gap-x-4">
+        <div className="bg-green-200 w-1/3">Maps</div>
+        <div className="bg-blue-500 w-full">
+          <div>graphic 1</div>
+          <div>graphic 2</div>
         </div>
+      </div>
     </div>
-  )
-}
-export default JourneyStatistics
+  );
+};
+export default JourneyStatistics;
