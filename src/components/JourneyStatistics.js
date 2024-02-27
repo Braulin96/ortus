61d78a2399791a92d30f55772edcc47034e8575b
Braulin Pires <braulinpires@hotmail.com>
2024-02-27 10:04:58 +0000

    created a new location component (not used yet)

--- src/components/JourneyStatistics.js
+++ src/components/JourneyStatistics.js
@@ -4,7 +4,6 @@ const JourneyStatistics = () => {
   return (
     <div className="w-100" style={{ width: "100%" }}>
       <div className="flex justify-between px-4 gap-x-4 py-8">
-       
         <div className="bg-green-200 w-1/3 h-80 overflow-hidden rounded-2xl opacity-90 hover:opacity-100 transition-opacity duration-500 ease-in-out">
           <Location />
         </div>
@@ -14,6 +13,7 @@ const JourneyStatistics = () => {
           <div>graphic 2</div>
         </div>
       </div>
+
       {/* <div className="bg-red-200 w-80 h-80">
         <Location />
       </div> */}
