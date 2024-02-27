5baf02d5d6da6408ed81700c830e69c6b477603e
Braulin Pires <braulinpires@hotmail.com>
2024-02-27 09:40:36 +0000

    added the smooth transition effect on opacity

--- src/components/JourneyStatistics.js
+++ src/components/JourneyStatistics.js
@@ -3,8 +3,12 @@ import Location from "./shared/Location";
 const JourneyStatistics = () => {
   return (
     <div className="w-100" style={{ width: "100%" }}>
-      <div className="flex justify-between px-4 gap-x-4">
-        <div className="bg-green-200 w-1/3 h-80 overflow-hidden"><Location /></div>
+      <div className="flex justify-between px-4 gap-x-4 py-8">
+       
+        <div className="bg-green-200 w-1/3 h-80 overflow-hidden rounded-2xl opacity-90 hover:opacity-100 transition-opacity duration-500 ease-in-out">
+          <Location />
+        </div>
+
         <div className="bg-blue-500 w-full">
           <div>graphic 1</div>
           <div>graphic 2</div>
