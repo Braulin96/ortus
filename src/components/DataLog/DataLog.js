816a607995391328fb02839f3b101fbf418e093f
Braulin Pires <braulinpires@hotmail.com>
2024-02-28 09:31:45 +0000

    created a separeted component to pick a date

--- src/components/DataLog/DataLog.js
+++ src/components/DataLog/DataLog.js
@@ -1,43 +1,40 @@
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker'
-import { DatePicker } from '@mui/x-date-pickers'
-//import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-
+import PickDate from "./shared/PickDate";
 
 const DataLog = () => {
   return (
     <>
-    <div
-      className="w-100 flex flex-col gap-y-4 mx-auto py-10 "
-      style={{ width: "100%", maxWidth: "1700px" }}
-    >
-      <div className="flex gap-x-8">
-        <div className="flex gap-x-2">
-          <p>From:</p>
-          <input type="number" className="border border-gray-500" />
-        </div>
+      <div
+        className="w-100 flex flex-col gap-y-4 mx-auto py-10 "
+        style={{ width: "100%", maxWidth: "1700px" }}
+      >
+        <div className="flex gap-x-8">
+          <div className="flex gap-x-2">
+            <p>From:</p>
+            <input type="number" className="border border-gray-500" />
+          </div>
 
-        <div className="flex gap-x-2">
-          <p>To:</p>
-          <input type="number" className="border border-gray-500" />
+          <div className="flex gap-x-2">
+            <p>To:</p>
+            <input type="number" className="border border-gray-500" />
+          </div>
+        </div>
+        <div className="flex gap-x-8">
+          <div className="flex gap-x-2">
+            <p>Start:</p>
+            <input type="number" className="border border-gray-500" />
+          </div>
+
+          <div className="flex gap-x-2">
+            <p>End:</p>
+            <input type="number" className="border border-gray-500" />
+          </div>
         </div>
       </div>
-      <div className="flex gap-x-8">
-        <div className="flex gap-x-2">
-          <p>Start:</p>
-          <input type="number" className="border border-gray-500" />
-        </div>
-
-        <div className="flex gap-x-2">
-          <p>End:</p>
-          <input type="number" className="border border-gray-500" />
-        </div>
+      <div className="">
+        <PickDate />
       </div>
-    </div>
 
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
 			<DemoContainer
 				sx={{
 					'& input': {
@@ -102,10 +99,7 @@ const DataLog = () => {
 					/>
 				</DemoItem>
 			</DemoContainer>
-		</LocalizationProvider>
-
-    
-    
+		</LocalizationProvider> */}
     </>
   );
 };
