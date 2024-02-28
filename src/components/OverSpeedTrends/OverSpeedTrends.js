b1f592bc31ac545a6a8dd890528ed97079da4c8c
Braulin Pires <braulinpires@hotmail.com>
2024-02-28 20:27:17 +0000

    added the other graph

--- src/components/OverSpeedTrends/OverSpeedTrends.js
+++ src/components/OverSpeedTrends/OverSpeedTrends.js
@@ -9,15 +9,25 @@ const OverSpeedTrends = () => {
       className="mx-auto py-10 p-4"
       style={{ width: "100%", maxWidth: "1700px" }}
     >
-      <div className="mb-8 flex gap-x-2">
+      <div className="mb-12 flex gap-x-2">
         <h2 className="text-3xl">Over Speed Trends</h2>
         <BsSpeedometer className="my-auto" color="gray" size={35} />
       </div>
+     
       <div className="flex md:flex-row flex-col w-full gap-x-4 gap-y-4">
         <div className="md:w-2/3 w-full rounded-2xl border-2 border-gray-100 shadow-lg p-2">
           <SpeedGpsGraph />
         </div>
         <div className="md:w-1/3 w-ful border-2 border-gray-100 shadow-lg rounded-xl">
+          <SwiperPagination />
+        </div>
+      </div>
+
+      <div className="flex md:flex-row flex-col w-full gap-x-4 gap-y-4 mt-8">
+        <div className="md:w-1/3 w-full rounded-2xl border-2 border-gray-100 shadow-lg p-2">
+          <SpeedGpsGraph />
+        </div>
+        <div className="md:w-2/3 w-ful border-2 border-gray-100 shadow-lg rounded-xl">
           <SwiperPagination />
         </div>
       </div>
