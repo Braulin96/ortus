6fbdca6722f82bee5ab6f9aabeb5e97ce8e5fc6f
Braulin Pires <braulinpires@hotmail.com>
2024-02-28 11:34:45 +0000

    removed the pickTime height

--- src/components/DataLog/shared/PickTime.js
+++ src/components/DataLog/shared/PickTime.js
@@ -30,7 +30,7 @@ function PickTime( {style}) {
 					'& .MuiInputBase-root': {
 						backgroundClip: 'padding-box',
 						fontFamily: 'inherit',
-						height: 'calc(1.5em + 0.75rem + 2px)',
+						//height: 'calc(1.5em + 0.75rem + 2px)',
 						padding: '0.375rem 0.75rem',
 						fontSize: '1rem',
 						fontWeight: '400',
