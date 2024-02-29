c00585be765d05797521811d60ad4859339d38bc
Braulin Pires <braulinpires@hotmail.com>
2024-02-29 16:29:41 +0000

    instaled aos fade on Srooll animation and created a shared component but not used yet

---
+++ src/components/shared/FadeOnScroll.js
@@ -0,0 +1,27 @@
+// Note: hooks
+import { useEffect } from "react";
+// Note: components:
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const FadeOnScroll = ({ data,duration, delay, children, offset, className }) => {
+  useEffect(() => {
+    AOS.init({
+      //disable: "phone",
+    });
+  }, []);
+    const offsetValue = offset || 200;
+  return (
+    <div
+        className={className}
+      data-aos="fade"
+      data-aos-offset={offsetValue}
+      data-aos-easing="ease-in-sine"
+      data-aos-duration={duration}
+      data-aos-delay={delay}
+    >
+      {children}
+    </div>
+  );
+};
+export default FadeOnScroll;
